9fba5b18a4eb02153836fe84cc34162394f617b5
10000585 <10000585.joan23@fje.edu>
2016-11-03 17:35:31 +0100

    index.php, login.php
    Login dia 3/11/16

--- includes/conexion_bd.php
+++ includes/conexion_bd.php
@@ -2,7 +2,7 @@
 	<?php
 
 		//realizamos la conexión
-		$conexion = mysqli_connect('localhost', 'root', '', 'bd_probike');
+		$conexion = mysqli_connect('localhost', 'root', '', 'bd_titanio');
 
 		//le decimos a la conexión que los datos los devuelva diréctamente en utf8, así no hay que usar htmlentities
 		$acentos = mysqli_query($conexion, "SET NAMES 'utf8'");
--- includes/login.php
+++ includes/login.php
@@ -1,38 +1,57 @@
 <?php
+//realizamos la conexión
+        $conexion = mysqli_connect('localhost', 'root', '', 'bd_titanio');
 
-include_once "includes/conexion.php";
+        //le decimos a la conexión que los datos los devuelva diréctamente en utf8, así no hay que usar htmlentities
+        $acentos = mysqli_query($conexion, "SET NAMES 'utf8'");
+
+         if (!$conexion) {
+             echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+             echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+             echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+             exit;
+         }
+
+
+// include_once "includes/conexion_bd.php";
+
    //CÓDIGO DE LOGIN:
+        extract($_REQUEST);
 
         //1.- Si existe la variable submit hacer:
-    if(isset($_POST['submit'])){
+    if(isset($submit)){
         
         //2.- Asignación de variables recibidas:
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
+        $usu_usuario = trim($username);
+        $usu_pwd = trim($password);
 
-        //3.- Si no es administrador:
-        if(!isset($_POST['admin'])){
+        if(!isset($admin)){
+            // echo "No existe <br/>";
+            $usu_categoria = 'profesor';
+        } else {
+            $usu_categoria = 'administrador';
+            // echo "Existe <br/>";
+        }
+        // $usu_categoria = $admin;
+
         
         //4.- Realización de consulta en la base de datos para los NO administradores:
+            $sql = "SELECT usu_usuario, usu_pwd, usu_categoria FROM  tbl_usuarios WHERE usu_usuario = '$usu_usuario' and usu_pwd = '$usu_pwd' and usu_categoria = '$usu_categoria'";
+            // echo $sql;
         
-            $records = $databaseConnection->prepare('SELECT username, password FROM  tbl_admgrups WHERE username = :username and password = :password');
-            $records->bindParam(':username', $username);
-            $records->bindParam(':password', $password);
-            $records->execute();
-            $results = $records->fetch(PDO::FETCH_ASSOC);
+
+            $login = mysqli_query($conexion, $sql);
 
 
-       } else {
 
-        //5.- Realización de consulta en la base de datos para los ADMINISTRADORES:
-        
-            $records = $databaseConnection->prepare('SELECT admin,password FROM  tbl_administradors WHERE admin = :username and password = :password');
-            $records->bindParam(':username', $username);
-            $records->bindParam(':password', $password);
-            $records->execute();
-            $results = $records->fetch(PDO::FETCH_ASSOC);
+            // $records = $databaseConnection->prepare('SELECT usu_usuario, usu_pwd, usu_categoria FROM  tbl_usuarios WHERE usu_usuario = :username and usu_pwd = :password and usu_categoria = :admin');
+            // $records->bindParam(':username', $usu_usuario);
+            // $records->bindParam(':password', $usu_pwd);
+            // $records->bindParam(':admin', $usu_categoria);
+            // $records->execute();
+            // $results = $records->fetch(PDO::FETCH_ASSOC);
+
 
        }
-    }
 
 ?>
