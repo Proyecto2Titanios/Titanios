3d0cc688dacf87b09d42c136146df26725f84e56
sergitnsr <sergitnsr@gmail.com>
2016-11-08 16:45:07 +0100

    Diseño recursos.php finalizado (xampp me da errores)
    Diseño finalizado.

--- recursos.php
+++ recursos.php
@@ -14,81 +14,30 @@ include "includes/login.php";
 <head>
   <meta charset="UTF-8">
   <title>Titanium Recursos</title>
-  
-  
-  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
 
-      <link rel="stylesheet" href="css/style.css">
-      <link rel="stylesheet" href="css/recursos.css">
+    <link rel="stylesheet" href="css/recursos.css">
 
-  
 </head>
 
 <body>
-  	<div class="espacios">
+  <div class="estadistica">
 
   			<div class="espaciosindiv"><img src="img/01.png"></div>
-  			
-  			<div class="espaciosindiv"><img src="img/02.png"></div>
-  			
+  			<div class="espaciosindiv"><img src="img/02.png"></div>	
   			<div class="espaciosindiv"><img src="img/03.png"></div>
-
   			<div class="espaciosindiv"><img src="img/04.png"></div>
-
-	</div>
-	<div class="espacios2">
-
-  			<div class="espaciosindiv"><img src="img/05.png"></div>
-  			
-  			<div class="espaciosindiv"><img src="img/06.png"></div>
-  			
-  			<div class="espaciosindiv"><img src="img/07.png"></div>
-
-  			<div class="espaciosindiv"><img src="img/08.png"></div>
-
-	</div>
-	<div class="espacios3">
-
-  			<div class="espaciosindiv"><img src="img/09.png"></div>
-  			
-  			<div class="espaciosindiv"><img src="img/no.png"></div>
-  			
-  			<div class="espaciosindiv"><img src="img/no.png"></div>
-
-  			<div class="espaciosindiv"><img src="img/no.png"></div>
-
-	</div>
-  <div class="material">
-
-        <div class="materialindiv"><img src="img/10.png"></div>
-        
-        <div class="materialindiv"><img src="img/11.png"></div>
-        
-        <div class="materialindiv"><img src="img/12.png"></div>
-
-        <div class="materialindiv"><img src="img/13.png"></div>
-
-  </div>
-  <div class="material2">
-
-        <div class="materialindiv"><img src="img/14.png"></div>
-        
-        <div class="materialindiv"><img src="img/15.png"></div>
-        
-        <div class="materialindiv"><img src="img/16.png"></div>
-
-        <div class="materialindiv"><img src="img/no.png"></div>
-
-  </div>
-  <div class="material3">
-
-        <div class="materialindiv"><img src="img/no.png"></div>
-        
-        <div class="materialindiv"><img src="img/no.png"></div>
-        
-        <div class="materialindiv"><img src="img/no.png"></div>
-
-        <div class="materialindiv"><img src="img/no.png"></div>
+        <div class="espaciosindiv"><img src="img/05.png"></div>
+        <div class="espaciosindiv"><img src="img/06.png"></div> 
+        <div class="espaciosindiv"><img src="img/07.png"></div>
+        <div class="espaciosindiv"><img src="img/08.png"></div>
+        <div class="espaciosindiv"><img src="img/09.png"></div>
+        <div class="espaciosindiv"><img src="img/10.png"></div> 
+        <div class="espaciosindiv"><img src="img/11.png"></div>
+        <div class="espaciosindiv"><img src="img/12.png"></div>
+        <div class="espaciosindiv"><img src="img/13.png"></div>
+        <div class="espaciosindiv"><img src="img/14.png"></div> 
+        <div class="espaciosindiv"><img src="img/15.png"></div>
+        <div class="espaciosindiv"><img src="img/16.png"></div>
 
   </div>
 </body>
