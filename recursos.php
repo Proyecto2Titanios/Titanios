75db82b23bf44adae25b5a3165110c355e5a7e9b
sergitnsr <sergitnsr@gmail.com>
2016-11-08 15:59:04 +0100

    Resize div bgs
    Redimensionado el fondo de los divs de la hoja de estilos recursos.css

--- recursos.php
+++ recursos.php
@@ -27,68 +27,68 @@ include "includes/login.php";
 <body>
   	<div class="espacios">
 
-  			<div class="espaciosindiv"><img src="img/mini/01.png"></div>
+  			<div class="espaciosindiv"><img src="img/01.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/02.png"></div>
+  			<div class="espaciosindiv"><img src="img/02.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/03.png"></div>
+  			<div class="espaciosindiv"><img src="img/03.png"></div>
 
-  			<div class="espaciosindiv"><img src="img/mini/04.png"></div>
+  			<div class="espaciosindiv"><img src="img/04.png"></div>
 
 	</div>
 	<div class="espacios2">
 
-  			<div class="espaciosindiv"><img src="img/mini/05.png"></div>
+  			<div class="espaciosindiv"><img src="img/05.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/06.png"></div>
+  			<div class="espaciosindiv"><img src="img/06.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/07.png"></div>
+  			<div class="espaciosindiv"><img src="img/07.png"></div>
 
-  			<div class="espaciosindiv"><img src="img/mini/08.png"></div>
+  			<div class="espaciosindiv"><img src="img/08.png"></div>
 
 	</div>
 	<div class="espacios3">
 
-  			<div class="espaciosindiv"><img src="img/mini/09.png"></div>
+  			<div class="espaciosindiv"><img src="img/09.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/no.png"></div>
+  			<div class="espaciosindiv"><img src="img/no.png"></div>
   			
-  			<div class="espaciosindiv"><img src="img/mini/no.png"></div>
+  			<div class="espaciosindiv"><img src="img/no.png"></div>
 
-  			<div class="espaciosindiv"><img src="img/mini/no.png"></div>
+  			<div class="espaciosindiv"><img src="img/no.png"></div>
 
 	</div>
   <div class="material">
 
-        <div class="materialindiv"><img src="img/mini/10.png"></div>
+        <div class="materialindiv"><img src="img/10.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/11.png"></div>
+        <div class="materialindiv"><img src="img/11.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/12.png"></div>
+        <div class="materialindiv"><img src="img/12.png"></div>
 
-        <div class="materialindiv"><img src="img/mini/13.png"></div>
+        <div class="materialindiv"><img src="img/13.png"></div>
 
   </div>
   <div class="material2">
 
-        <div class="materialindiv"><img src="img/mini/14.png"></div>
+        <div class="materialindiv"><img src="img/14.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/15.png"></div>
+        <div class="materialindiv"><img src="img/15.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/16.png"></div>
+        <div class="materialindiv"><img src="img/16.png"></div>
 
-        <div class="materialindiv"><img src="img/mini/no.png"></div>
+        <div class="materialindiv"><img src="img/no.png"></div>
 
   </div>
   <div class="material3">
 
-        <div class="materialindiv"><img src="img/mini/no.png"></div>
+        <div class="materialindiv"><img src="img/no.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/no.png"></div>
+        <div class="materialindiv"><img src="img/no.png"></div>
         
-        <div class="materialindiv"><img src="img/mini/no.png"></div>
+        <div class="materialindiv"><img src="img/no.png"></div>
 
-        <div class="materialindiv"><img src="img/mini/no.png"></div>
+        <div class="materialindiv"><img src="img/no.png"></div>
 
   </div>
 </body>
